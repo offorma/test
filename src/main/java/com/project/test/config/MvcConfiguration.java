93d50877dc775bbcd44a77615392fa08d5c68aa4
Chukwunonso <offorma@gmail.com>
2017-03-04 22:04:19 +0000

    working with the help of Farouk

--- src/main/java/com/project/test/config/MvcConfiguration.java
+++ src/main/java/com/project/test/config/MvcConfiguration.java
@@ -1,22 +1,16 @@
 package com.project.test.config;
-
-
 import java.util.Properties;
 
-import javax.annotation.Resource;
 import javax.sql.DataSource;
 
-import org.hibernate.ejb.HibernatePersistence;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -24,69 +18,56 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-@EnableTransactionManagement
 @ComponentScan(basePackages="com.project.test")
 @EnableWebMvc
-@PropertySource("classpath:application.properties")
-@EnableJpaRepositories("com.project.test.repository")
-
 public class MvcConfiguration extends WebMvcConfigurerAdapter{
-    private static final String PROPERTY_NAME_DATABASE_DRIVER = "db.driver";
-    private static final String PROPERTY_NAME_DATABASE_PASSWORD = "db.password";
-    private static final String PROPERTY_NAME_DATABASE_URL = "db.url";
-    private static final String PROPERTY_NAME_DATABASE_USERNAME = "db.username";
- 
-    private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "hibernate.dialect";
-    private static final String PROPERTY_NAME_HIBERNATE_SHOW_SQL = "hibernate.show_sql";
-    private static final String PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN = "entitymanager.packages.to.scan";
-    @Resource
-    private Environment env;
+
 	@Bean
 	public ViewResolver getViewResolver(){
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
 		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
-		
 		return resolver;
 	}
-	@Override// this tells me where i can find all my static resources
+	
+	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
 	}
 	
-	@Bean
-    public DataSource dataSource() {
+	@Bean(name = "dataSource")
+    public DataSource getDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(env.getRequiredProperty(PROPERTY_NAME_DATABASE_DRIVER));
-        dataSource.setUrl(env.getRequiredProperty(PROPERTY_NAME_DATABASE_URL));
-        dataSource.setUsername(env.getRequiredProperty(PROPERTY_NAME_DATABASE_USERNAME));
-        dataSource.setPassword(env.getRequiredProperty(PROPERTY_NAME_DATABASE_PASSWORD));
- 
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/mscproject_data");
+        dataSource.setUsername("root");
+        dataSource.setPassword(null);
         return dataSource;
     }
-	
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
-        entityManager.setDataSource(dataSource());
-        entityManager.setPersistenceProviderClass(HibernatePersistence.class);
-        entityManager.setPackagesToScan(env.getRequiredProperty(PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN));
-        entityManager.setJpaProperties(hibProperties());
-         
-        return entityManager;
-    }
- 
-    private Properties hibProperties() {
-        Properties properties = new Properties();
-        properties.put(PROPERTY_NAME_HIBERNATE_DIALECT, env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_DIALECT));
-        properties.put(PROPERTY_NAME_HIBERNATE_SHOW_SQL, env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_SHOW_SQL));
-        return properties;
-    }
- 
-    @Bean
-    public JpaTransactionManager transactionManager() {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
-        return transactionManager;
-    }
+	 private Properties getHibernateProperties() {
+	        Properties properties = new Properties();
+	        properties.put("hibernate.show_sql", "true");
+	        properties.put("hibernate.dialect",
+	                "org.hibernate.dialect.MySQLDialect");
+	        properties.put("hibernate.hbm2ddl.auto", "update");
+	        return properties;
+	    }
+
+	    @Bean(name = "sessionFactory")
+	    public SessionFactory getSessionFactory(DataSource dataSource) {
+	        LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(
+	                dataSource);
+	        sessionBuilder.addProperties(getHibernateProperties());
+	        sessionBuilder.scanPackages("com.project.test.model");
+	        return sessionBuilder.buildSessionFactory();
+	    }
+	    @Bean(name="transactionManger")
+	    @Autowired
+	    public HibernateTransactionManager getTransactionManager(
+	            SessionFactory sessionFactory) {
+	        HibernateTransactionManager transactionManager = new HibernateTransactionManager(
+	                sessionFactory);
+	        return transactionManager;
+	    }
+
 }
