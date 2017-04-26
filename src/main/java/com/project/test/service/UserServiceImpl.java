86506b0d3363503afd6c751b5ce1e37e5df1abb6
Chukwunonso <offorma@gmail.com>
2017-04-26 23:13:30 +0100

    add service annotation to user service

--- src/main/java/com/project/test/service/UserServiceImpl.java
+++ src/main/java/com/project/test/service/UserServiceImpl.java
@@ -2,10 +2,14 @@ package com.project.test.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.project.test.model.User;
 import com.project.test.repositories.UserRepositoryImpl;
-
+@Service
 public class UserServiceImpl implements UserService {
+	@Autowired
 	UserRepositoryImpl userRepo;
 
 	@Override
